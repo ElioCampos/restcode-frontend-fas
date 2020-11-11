c0b64322bd1af11878440c4419f19dfddf5a95dc
Lucero Gejaño <anlugeri2001@gmail.com>
2020-11-11 16:33:56 -0500

    Appointment Frontend

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <v-app>
+    <v-app-bar app dark fixed color="#1bd698">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>RestCode</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text to="/">Inicio</v-btn>
+        <v-btn text to="/appointments">Citas</v-btn>
+        <v-btn text to="/about">Acerca de</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 
+
 export default {
   name: 'App',
 
   components: {
+
   },
 
   data: () => ({
