b18a0e2a325a19851eb36dfcfe93147266698ae4
FloresIrene <71415300+FloresIrene@users.noreply.github.com>
2020-11-26 09:51:33 -0500

    Update restaurants.vue

--- src/components/restaurants.vue
+++ src/components/restaurants.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-title class="justify-center">
-      RESTAURANTE {{defaultItem.name.toUpperCase()}}
+      <h3>RESTAURANTE {{defaultItem.name.toUpperCase()}}</h3>
     </v-card-title>
     <div class="d-flex flex-column justify-space-between align-center">
       <v-img
@@ -24,6 +24,29 @@
         Editar Información
       </v-btn>
     </v-col>
+    <v-card-title class="justify-center">
+      <h5>GALERÍA DE FOTOS</h5>
+    </v-card-title>
+    <v-row>
+      <v-col
+          v-for="item in defaultItem.photo"
+          :key="item.id"
+          cols="4"
+      >
+        <v-img
+            :src="item.src"
+            aspect-ratio="1"
+        >
+          <template v-slot:placeholder>
+            <v-row
+                align="center"
+                justify="center"
+            >
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
@@ -39,7 +62,8 @@ export default {
         name: '',
         address: '',
         cellPhoneNumber: 0,
-        ownerId: 0
+        ownerId: 0,
+        photo: []
       }
     }
   },
@@ -54,6 +78,32 @@ export default {
       RestaurantService.get(id)
           .then(response => {
             this.defaultItem = response.data;
+            this.defaultItem.photo = [
+              {
+                id: 1,
+                src: 'https://cdn.pixabay.com/photo/2015/06/30/18/36/st-826688_960_720.jpg'
+              },
+              {
+                id: 2,
+                src: 'https://cdn.pixabay.com/photo/2014/09/17/20/26/restaurant-449952__340.jpg'
+              },
+              {
+                id: 3,
+                src: 'https://cdn.pixabay.com/photo/2020/01/31/07/26/chef-4807317_960_720.jpg'
+              },
+              {
+                id: 4,
+                src: 'https://cdn.pixabay.com/photo/2016/04/02/17/58/service-1303313_960_720.jpg'
+              },
+              {
+                id: 5,
+                src: 'https://cdn.pixabay.com/photo/2016/03/27/21/34/restaurant-1284351_960_720.jpg'
+              },
+              {
+                id: 6,
+                src: 'https://cdn.pixabay.com/photo/2015/03/26/10/28/restaurant-691397__340.jpg'
+              }
+            ]
           })
           .catch((e) => {
             console.log(e);
